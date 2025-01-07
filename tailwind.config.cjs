303dbacca9cdf31346dfcb7b8961ba0ae8563675
Bilal Iqbal <135320038+bil01aliq123bal@users.noreply.github.com>
2025-01-07 14:48:43 +0500

    Add files via upload

---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Montserrat: "Montserrat",
+      },
+      colors: {
+        'custom-color': '#202020',
+      }
+    },
+  },
+  plugins: [require("tailwind-scrollbar")],
+};
